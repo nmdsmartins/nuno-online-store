90f6757ca6aedf60ab17cf52152f235db4894408
Nuno Martins <nmdsmartins@gmail.com>
2022-12-02 14:30:46 +0000

    Adding simple app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.json({
+        baseUrl: req.baseUrl,
+        url: req.url,
+        method: req.method,
+        path: req.path,
+        headers: req.headers,
+        query: req.query    ,
+        cookies: req.cookies,
+        mongodbUrl: process.env.DB_URL ?? "not found"
+    });
+    next();
+});
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
